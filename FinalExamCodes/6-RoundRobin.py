a9a6016dc1584a7f8fd6763ff9f3d909ae6cbac8
Anuj Gholve <anujgholve@gmail.com>
2025-11-12 14:26:06 +0530

    Enhance round-robin scheduling with arrival times
    Added arrival time input and adjusted scheduling logic to handle arrival times in the round-robin algorithm.

--- FinalExamCodes/6-RoundRobin.py
+++ FinalExamCodes/6-RoundRobin.py
@@ -1,32 +1,52 @@
 n = int(input("Enter number of processes: "))
-bt = []
-for i in range(n):
-    bt.append(int(input(f"Enter Burst Time of P{i+1}: ")))
-tq = int(input("Enter Time Quantum: "))
 
-rem = bt[:]
+at = []  # Arrival times
+bt = []  # Burst times
+for i in range(n):
+    at.append(int(input(f"Enter Arrival Time of P{i+1}: ")))
+    bt.append(int(input(f"Enter Burst Time of P{i+1}: ")))
+
+tq = int(input("Enter Time Quantum: "))
+rem = bt[:] 
 t = 0
 wt, tat = [0]*n, [0]*n
-done = 0
-while True:
-    flag = True
-    for i in range(n):
-        if rem[i] > 0:
-            flag = False
-            if rem[i] > tq:
-                t += tq
-                rem[i] -= tq
-            else:
-                t += rem[i]
-                wt[i] = t - bt[i]
-                rem[i] = 0
-                tat[i] = t
-    if flag:
-        break
+ready_queue = []
+visited = [False]*n
+completed = 0
 
-print("\nProcess\tBT\tWT\tTAT")
+while completed < n:
+    for i in range(n):
+        if at[i] <= t and not visited[i]:
+            ready_queue.append(i)
+            visited[i] = True
+
+    if not ready_queue:
+        t += 1  
+        continue
+
+    i = ready_queue.pop(0)  
+
+    if rem[i] > tq:
+        rem[i] -= tq
+        t += tq
+    else:
+        t += rem[i]
+        rem[i] = 0
+        tat[i] = t - at[i]
+        wt[i] = tat[i] - bt[i]
+        completed += 1
+
+    for j in range(n):
+        if at[j] <= t and not visited[j]:
+            ready_queue.append(j)
+            visited[j] = True
+
+    if rem[i] > 0:
+        ready_queue.append(i)
+
+print("\nProcess\tAT\tBT\tWT\tTAT")
 for i in range(n):
-    print(f"P{i+1}\t{bt[i]}\t{wt[i]}\t{tat[i]}")
+    print(f"P{i+1}\t{at[i]}\t{bt[i]}\t{wt[i]}\t{tat[i]}")
 
 # Sample Output:
 # Enter number of processes: 3
@@ -39,3 +59,4 @@ for i in range(n):
 # P1	        5	4	9
 # P2	        3	4	7
 # P3	        1	2	3
+
